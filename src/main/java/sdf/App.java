52cb2b49a242a17088fa322e0d1657f8d7d4f0a4
bryce-88 <yengpeng.khor@gmail.com>
2023-01-17 14:52:23 +0800

    redone Server app

--- src/main/java/sdf/App.java
+++ src/main/java/sdf/App.java
@@ -1,6 +1,13 @@
 package sdf;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Hello world!
@@ -13,7 +20,7 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String dirPath = "data2";
 
@@ -29,5 +36,29 @@ public final class App {
             newDirectory.mkdir();
         }
         
+
+        Cookie cookie = new Cookie();
+        cookie.readCookieFile();
+        cookie.showCookies();
+
+        ServerSocket ss = new ServerSocket(7000);
+        Socket s = ss.accept(); //establish connection, wait for client to connect
+
+        try (InputStream is = s.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+            String msgReceived = "";
+
+            while (!msgReceived.equals("close")) {
+                msgReceived = dis.readUTF();
+
+                if (msgReceived.equalsIgnoreCase("get-cookie")) {
+                    String cookieValue = cookie.returnCookie();
+                }
+            }
+        } catch (EOFException ex) {
+            s.close();
+            ss.close();
+        }
     }
 }
--- src/main/java/sdf/Cookie.java
+++ src/main/java/sdf/Cookie.java
@@ -1,17 +1,9 @@
 package sdf;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.EOFException;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -43,33 +35,8 @@ public class Cookie {
             br.close();
             fr.close();
         }
-
-
-        Cookie cookie = new Cookie();
-        cookie.readCookieFile();
-        cookie.showCookies();
-
-        ServerSocket ss = new ServerSocket(7000);
-        Socket s = ss.accept(); //establish connection, wait for client to connect
-
-        try (InputStream is = s.getInputStream()) {
-            BufferedInputStream bis = new BufferedInputStream(is);
-            DataInputStream dis = new DataInputStream(bis);
-            String msgReceived = "";
-
-            while (!msgReceived.equals("close")) {
-                msgReceived = dis.readUTF();
-
-                if (msgReceived.equalsIgnoreCase("get-cookie")) {
-                    String cookieValue = cookie.returnCookie();
-                }
-            }
-        } catch (EOFException ex) {
-            s.close();
-            ss.close();
-        }
     }
-
+        
     public String returnCookie() {
         Random rand = new Random();
 
